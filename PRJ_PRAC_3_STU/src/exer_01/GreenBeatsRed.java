3cf99050b78ed866b99f9ddb80ae5ff8e5f2bc88
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2024-10-30 22:01:12 +0100

    Update GreenBeatsRed.java

--- PRJ_PRAC_3_STU/src/exer_01/GreenBeatsRed.java
+++ PRJ_PRAC_3_STU/src/exer_01/GreenBeatsRed.java
@@ -27,51 +27,98 @@ public class GreenBeatsRed {
 
 
 
-playTheGame = true;
+        playTheGame = true;
         while (playTheGame) { 
         	gameNumber ++; 
-        	 console.println("GAME #" + gameNumber + "Spent: " + moneySpent + "Won: " + moneyWon );
+        	moneySpent= moneySpent+6;
+        	 console.print("GAME #");
+        	 console.setForegroundColor(Color.YELLOW);
+        	 console.print(gameNumber);
+        	 console.resetColor();
+        	 console.print(" Spent: ");
+        	 console.setForegroundColor(Color.RED);
+        	 console.print(moneySpent);
+        	 console.resetColor();
+        	 console.print(" Won: " );
+        	 console.setForegroundColor(Color.GREEN);
+        	 console.println (moneyWon);
+        	 console.resetColor();
+        	 console.println("");
         	 cast=1;
          	 console.println ("Game is about to start. Press any key to roll your dice");
+         	 console.println("");
       	 
          	 while(cast<=3) {
          		 	console.readKey();
-         		 	console.println("CAST #" + cast + "\tGeen\tRed\tRed");
-         		 	console.println("\t\t-----\t-----\t-----");
+         		 	console.print("CAST #"); 
+         		 	console.setForegroundColor(Color.YELLOW);
+         		 	console.print(cast); 
+         		 	console.resetColor();
+         		 	console.println("\tGeen\tRed\tRed");
+         		 	console.println("\t\t -----\t-----\t-----");
          		 	roll1 = alea.nextInt(1, 7);
         			roll2 = alea.nextInt(1, 7);
         			roll3 = alea.nextInt(1, 7);
         			console.println("\t\t" + roll1 + "\t" + roll2 + "\t" + roll3);
         	
         				if (roll1 >= Math.max(roll2, roll3)) {
+        					console.println("");
         					console.println("GOOD, green beats the higher red. Press any key to roll the dice again");
+        					console.println("");
         				}
         
         				else {
-        					console.println("NOPE, green does not beat higher red. Gambler, game #" + gameNumber + " ends here");
-        		
+        					console.println("");
+        					console.println("\t NOPE, green does not beat higher red. Gambler, game #" + gameNumber + " ends here");
+        					console.println("");
+        					console.setForegroundColor(Color.RED);
+        					console.print("OH PITTY ");
+        					console.resetColor();
+        					console.println("green die couldn't do it three times");
         				}
         	
         				if (roll1<roll2 && cast==1  || roll1<roll3 && cast ==1 ) {
-        					console.println("OH PITY");
+
+        					console.println("it didn't do it even once. YOU WIN NOTHING");
         					cast=4;
+        					playTheGame = false;
+        				}
+        				
+        				else if (roll1<roll2 && cast==2 || roll1<roll3 && cast==2){
+        					console.println("but it manage to do it ONCE. YOU WIN TWO bucks");
+        					moneyWon += 2;
+        					cast=4;
+        					playTheGame = false;
+        				}
+        				else if (roll1<roll2 && cast==3 || roll1<roll3 && cast==3) {
+        					console.println("but it manage to do it TWICE. YOU WIN THREE bucks");
+        					moneyWon += 3;
+        					cast=4;
+        					playTheGame = false;
         				}
         				
         				
         				if (cast==3) {
-        					console.println("Excelent! Green won all three casts! You win $60.");
+        					console.println("Excelent! green die BEASTED the red ones THREE times");
+        					console.println("You win 60 bucks");
         					moneyWon += 60;
         					playTheGame = false;
         				}
         				else {
         					console.print("");
         				}
+        				
         	cast++;
       	 } 	
+         	 console.println("");
+         	 console.println("");
              console.println("Play again? (y/Y or n/N)");
+             console.setForegroundColor(Color.GREEN);
              answer = console.readChar();
+             console.resetColor();
              if (answer == 'y' || answer == 'Y') {
              	playTheGame = true;
+             	console.clear();
              	
              	}
              	
@@ -80,12 +127,15 @@ playTheGame = true;
              		}
              			else {
              				while(answer !='y'&& answer !='Y'&& answer !='n'&& answer !='N') {
-             						
+             						console.setForegroundColor(Color.RED);
              						console.println("Wrong answer");
-             				answer = console.readChar();
+             						console.setForegroundColor(Color.RED);
+             			             answer = console.readChar();
+             			             console.resetColor();
+      
              				}
              			}
-  		 }
+   }
 
 	// End of the outer iteration
         // why has this point been reached?
